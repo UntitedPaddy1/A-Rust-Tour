58e00ab48766b064e54388bf8ff9b5e526597fa5
UnitedPaddy1 <180303324+UntitedPaddy1@users.noreply.github.com>
2024-09-03 17:29:54 +0200

    A Basic Hello World! Written in Rust

---
+++ hello-rust/target/debug/deps/hello_rust-07ed028adebdda79.d
@@ -0,0 +1,5 @@
+/home/stefy/RustroverProjects/A-Rust-Tour/hello-rust/target/debug/deps/hello_rust-07ed028adebdda79: src/main.rs
+
+/home/stefy/RustroverProjects/A-Rust-Tour/hello-rust/target/debug/deps/hello_rust-07ed028adebdda79.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/stefy/RustroverProjects/A-Rust-Tour/hello-rust/target/debug/hello-rust: /home/stefy/RustroverProjects/A-Rust-Tour/hello-rust/src/main.rs
